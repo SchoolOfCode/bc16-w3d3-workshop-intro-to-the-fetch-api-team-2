7b0718b719fee6640a13efe133bc13f094ac7531
arseniyus <r.kozachenko2000@gmail.com>
2024-02-21 16:11:54 +0000

    removed uneccesary function

--- app.js
+++ app.js
@@ -22,18 +22,15 @@ console.log(data.joke);
 const jokeElement = document.getElementById("joke");
 jokeElement.textContent = data.joke;
 }
-// Main function to retrieve and display a new joke
-function getAndDisplayNewJoke() {
-  const joke = fetchJoke();
-}
+
+
 // Waits for the DOM to be fully loaded and then displays an initial joke.
-document.addEventListener("DOMContentLoaded", getAndDisplayNewJoke);
+document.addEventListener("DOMContentLoaded", fetchJoke);
 
 // Retrieves the "new joke" button
 const newJokeButton = document.getElementById("newJokeBtn");
 
 // Sets up a click event listener to fetch and display a new joke upon clicking the newJokeButton.
-newJokeButton.addEventListener("click", getAndDisplayNewJoke);
-
+newJokeButton.addEventListener("click", fetchJoke);
 // We have every joke used printed in console.log
 // Get console.logged jokes and display them in a scrollable window called history
