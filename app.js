da6d5c5294f85e04776e3dbf1f25739e3482f2cc
arseniyus <r.kozachenko2000@gmail.com>
2024-02-21 15:27:57 +0000

    done and dusted MVPs are blasted

--- app.js
+++ app.js
@@ -1,38 +1,31 @@
 // Hardcoded array of joke objects
 // Each joke object has a unique id
-const jokes = [
-  {
-    id: "R7UfaahVfFd",
-    joke: "My dog used to chase people on a bike a lot. It got so bad I had to take his bike away.",
-  },
-  {
-    id: "x2plLdX",
-    joke: "Why did the scarecrow win an award? Because he was outstanding in his field!",
-  },
-  {
-    id: "a7Ub0avj0Ba",
-    joke: "Why don't skeletons fight each other? They don't have the guts.",
-  },
-];
 
+const url = "https://icanhazdadjoke.com"
+
+async function fetchJoke() {
+
+const response = await fetch(url, {
+  headers: {
+    Accept: "application/json", 
+  },
+});
+
+if (!response.ok) {
+   console.error(response.status);
+   console.error(response.text());
+   return;
+}
+const data = await response.json();
+
+console.log(data.joke);
+const jokeElement = document.getElementById("joke");
+jokeElement.textContent = data.joke;
+}
 // Main function to retrieve and display a new joke
 function getAndDisplayNewJoke() {
-  const joke = retrieveJoke();
-  displayJoke(joke);
+  const joke = fetchJoke();
 }
-
-// Function to retrieve a random joke
-function retrieveJoke() {
-  const randomIndex = Math.floor(Math.random() * jokes.length);
-  return jokes[randomIndex];
-}
-
-// Function to update the DOM with the provided joke
-function displayJoke(joke) {
-  const jokeElement = document.getElementById("joke");
-  jokeElement.textContent = joke.joke;
-}
-
 // Waits for the DOM to be fully loaded and then displays an initial joke.
 document.addEventListener("DOMContentLoaded", getAndDisplayNewJoke);
 
@@ -41,3 +34,6 @@ const newJokeButton = document.getElementById("newJokeBtn");
 
 // Sets up a click event listener to fetch and display a new joke upon clicking the newJokeButton.
 newJokeButton.addEventListener("click", getAndDisplayNewJoke);
+
+// We have every joke used printed in console.log
+// Get console.logged jokes and display them in a scrollable window called history
